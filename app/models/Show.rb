5d7f41e5d0b2983a5a169d59f7753d15c59d78c1
Mike <mclancy96@gmail.com>
2025-07-25 12:11:14 -0400

    Ratings methods

--- app/models/Show.rb
+++ app/models/Show.rb
@@ -1,2 +1,17 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.order(rating: :desc).first
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.order(rating: :asc).first
+  end
 end

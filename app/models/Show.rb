dc689297b9be22f92f6719bccf0c3dbe3cce748f
Mike <mclancy96@gmail.com>
2025-07-25 12:13:53 -0400

    Sum, popular, and order

--- app/models/Show.rb
+++ app/models/Show.rb
@@ -14,4 +14,16 @@ class Show < ActiveRecord::Base
   def self.least_popular_show
     Show.order(rating: :asc).first
   end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where('rating > 5')
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
 end
